0b30f3b896b0ccdaa8f496738cf81045ec1d0f4c
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-05-19 00:51:36 -0400

    Version 1.0.0

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 // middleware
 app.use(cors());
+app.use(express.static('public'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
---
+++ public/precache-manifest.0c30f04387b213471ed62919d49abdf7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295479d2cfaee1833c4f50866b3f0fc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0898b5be54fd4ba83b0e",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f41f3b7372951be30727",
+    "url": "/static/css/main.1745befb.chunk.css"
+  },
+  {
+    "revision": "0898b5be54fd4ba83b0e",
+    "url": "/static/js/2.0b224693.chunk.js"
+  },
+  {
+    "revision": "3fb580b14673f072ae0cc33a1446ad57",
+    "url": "/static/js/2.0b224693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41f3b7372951be30727",
+    "url": "/static/js/main.2331aef7.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "840515e0b5963294e6fd559986787798",
+    "url": "/static/media/background-v2.840515e0.jpg"
+  },
+  {
+    "revision": "8ef0f68cf32150b33b9c16a369045d4d",
+    "url": "/static/media/canvasBackground.8ef0f68c.jpg"
+  },
+  {
+    "revision": "8ecc35e375b4878d94c3988843f1e7c3",
+    "url": "/static/media/mona-lisa.8ecc35e3.jpg"
+  },
+  {
+    "revision": "8bc9bc2913433f8c73fc37baf2ebb8bd",
+    "url": "/static/media/source2.8bc9bc29.gif"
+  }
+]);
